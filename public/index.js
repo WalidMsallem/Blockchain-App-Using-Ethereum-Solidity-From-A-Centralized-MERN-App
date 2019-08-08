b63bdc7c437c1e3dc2815b645aa2c3baf06f0b72
Walid <walidmsallem@gmail.com>
2019-08-08 15:36:52 +0100

    post with axios

--- public/index.js
+++ public/index.js
@@ -2,8 +2,18 @@ class App extends React.Component  {
   constructor(props) {
           super(props)
     this.state = {
-    total_amount : 1000
+    total_amount : 1000,
+    amount : 100,
+    email : ''
     }
+  }
+  onSubmit = async (event ) => {
+    event.preventDefault()
+    const response = await axios.post('/post_info', {
+      amount : this.state.amount ,
+      email : this.state.email
+    })
+    console.log(response)
   }
   render () {
     return(
@@ -12,10 +22,10 @@ class App extends React.Component  {
          <div>
           <p>Total lettery amount is  {this.state.total_amount}</p>
 
-               <form>
-                <input placeholder="amount" name=""/>
-                 <input placeholder="email" name="" />
-                 <button> participate </button>
+               <form onSubmit = {this.onSubmit}>
+                <input placeholder="amount" name="" value = {this.state.amount} onChange = {event => this.setState({amount:event.target.value})}/>
+                 <input placeholder="email" name=""   value = {this.state.email}  onChange = {event => this.setState({email:event.target.value})} />
+                 <button type = "submit"> participate </button>
               </form>
        </div>
      </div>
